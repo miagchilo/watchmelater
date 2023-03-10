8d828e683a5bb7634f014254eb8275762c91fb13
Stan Miagchilo <stasss95@mail.ru>
2023-03-10 06:44:21 +0100

    delete som files

--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-function Header() {
-    return (
-     <h1>Watch me later tube</h1>
-    );
-  };
-  
-  export default Header;
